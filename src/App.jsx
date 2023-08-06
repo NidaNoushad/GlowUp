58baa0e0928bc0064de38340a7d687b358e731ea
Nida Noushad <nida11noushad@gmail.com>
2023-08-06 13:43:25 +0530

    secondChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../src/Component/Header'
+import MainSection from './Component/MainSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Header/>
-     
+     <MainSection/>
     </>
   )
 }
