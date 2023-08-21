582763c6b3256a4a8224f8456b213a65dbf83f45
Nida Noushad <nida11noushad@gmail.com>
2023-08-21 14:12:04 +0530

    footerreact added

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import css from './Footer.module.css'
+import LogoImage from '../Images/Logo1.png';
+import { CiLocationOn } from 'react-icons/ci';
+import { IoCallOutline } from 'react-icons/io5';
+import {GoSignIn,GoPeople,GoLink} from 'react-icons/go'; 
+
+const Footer = () => {
+  return (
+    <div className='container-fluid'>
+      <hr />
+      <div className='row m-2'>
+        <div className={`${css.footeralign} col-2`}>
+          <div className={css.imgbox}>
+          <a href="" style={{textDecoration:"none",color:"white"}}>
+            <img src={LogoImage} alt="logo" style={{width:"100%"}} /></a>
+          </div>
+        </div>
+        <div className={`${css.footeralign} col-3`}>
+          <ul className={css.footerlist}>
+            <li className='mb-3 fw-bold'>Contact Us</li>
+            <li>
+            <a href="" style={{textDecoration:"none",color:"white"}}>
+              <span className={css.reacticon}><CiLocationOn /></span>
+              <span className={css.footerinfo}>111 north avenue Orlando, FL 32801</span></a>
+            </li>
+            <li>
+            <a href="" style={{textDecoration:"none",color:"white"}}>
+              <span className={css.reacticon}><IoCallOutline /></span>
+              <span className={css.footerinfo}>352-306-4415</span></a>
+            </li>
+
+          </ul>
+        </div>
+        <div className={`${css.footeralign} col-2 text-center`}>
+          <ul className={css.footerlist}>
+            <li className='mb-3 fw-bold'>Account</li>
+            <li>
+            <a href="" style={{textDecoration:"none",color:"white"}}>
+              <span className={css.reacticon}><GoSignIn/></span>
+              <span className={css.footerinfo}>Sign In</span></a>
+            </li>
+          </ul>
+        </div>
+        <div className={`${css.footeralign} col-2  text-center`}>
+          <ul className={css.footerlist}>
+            <li className='mb-3 fw-bold'>Company</li>
+            <li>
+            <a href="" style={{textDecoration:"none",color:"white"}}>
+              <span className={css.reacticon}><GoPeople/></span>
+              <span className={css.footerinfo}>About us</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={`${css.footeralign} col-3 text-center`}>
+          <ul className={css.footerlist}>
+            <li className='mb-3 fw-bold'>Resources</li>
+            <li>
+              <a href="" style={{textDecoration:"none",color:"white"}}>
+              <span className={css.reacticon}><GoLink/></span>
+              <span className={css.footerinfo}>Safety Privacy & Terms</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='text-center ' style={{fontSize:"10px"}}>
+        <p className='mb-0'>Copyright ©2023 by GlowUp, Inc.</p>
+           <p className='mb-0'>All rights reserved.</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
